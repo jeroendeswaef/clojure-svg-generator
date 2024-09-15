3c46511167c35a7c8fd6e73fdc1abe460a425f51
Jeroen De Swaef <j@recallq.com>
2024-09-15 11:20:21 +0200

    wip: refactor to allow for different step kinds

--- src/make_svg/core.cljs
+++ src/make_svg/core.cljs
@@ -1,4 +1,5 @@
 (ns make-svg.core
+  (:require [clojure.string :refer [join]])
   (:require [clojure.data.xml :as xml])
   (:require [clojure.math :as math]))
 
@@ -28,15 +29,26 @@
      :y (format "%.2f" (* r (math/sin (math/to-radians angle)))) }
 )
 (defn coords-str-at-angle [r angle] (let [coords (coords-at-angle r angle)] (str (get coords :x) " " (get coords :y))))
-(def steps (map (fn [step-i] { :angle (/ (* step-i 90) (- step-cnt 1))} ) (range 0 step-cnt)))
-(def combined-path (apply str (map (fn [step] (str "M " (coords-str-at-angle inner-radius (get step :angle)) " L " (coords-str-at-angle outer-radius (get step :angle)) " z ")) steps)))
+;;(def steps (map (fn [step-i] [{ :angle (/ (* step-i 90) (- step-cnt 1)), :kind :cross}] ) (range 0 step-cnt)))
+(def steps (reduce (fn [state input] (
+  let [angle (/ (* input 90) (- step-cnt 1))] 
+  (((remove nil?) conj) (conj state { :kind :cross, :angle angle }) ( if (< (/ (count state) 2) (- step-cnt 1)) { :kind :bind })) 
+)) [] (range 0 step-cnt)))
+(println "steps:" steps)
+(defn combined-path [steps]
+  (apply str (map (fn[step] (case (get step :kind)
+    :cross (join " " [(coords-str-at-angle inner-radius (get step :angle)) "L" (coords-str-at-angle outer-radius (get step :angle))])
+    :bind "-"
+  )) steps))
+)
+;;(def combined-path (str "M 0 0 " (apply str (map (fn [step] (str "L " (coords-str-at-angle inner-radius (get step :angle)) " L " (coords-str-at-angle outer-radius (get step :angle)) " ")) steps)) " z"))
 
-(println combined-path)
-(def hinge-path combined-path)
+(println (combined-path steps))
+;;(def hinge-path combined-path)
 
-(defn write-rss! [xml]
-  (with-open [out-file (java.io.FileWriter. "out.svg")]
-    (xml/emit xml out-file)))
+;;(defn write-rss! [xml]
+;;   (with-open [out-file (java.io.FileWriter. "out.svg")]
+;;     (xml/emit xml out-file)))
 
-(-> (generate-svg-xml hinge-path)
-    write-rss!)
+;; (-> (generate-svg-xml hinge-path)
+;;     write-rss!)
