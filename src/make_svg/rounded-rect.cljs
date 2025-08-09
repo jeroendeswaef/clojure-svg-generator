95a33b04595d7c04a6bc7d6ff9dc6b438fb0773d
Jeroen De Swaef <j@recallq.com>
2025-08-09 11:52:37 +0200

    generalize stuff

--- src/make_svg/rounded-rect.cljs
+++ src/make_svg/rounded-rect.cljs
@@ -3,13 +3,21 @@
   (:require [clojure.data.xml :as xml])
   (:require [clojure.math :as math]))
 
+(def border-radius 5)
+(def padding 7)
+(def width 40)
+(def height 30)
+
+(def document-width (+ width (* 2 padding)))
+(def document-height (+ height (* 2 padding)))
+
 (defn generate-svg-xml [svg-path]
   (xml/sexp-as-element
    [:svg
     {:version "1.1"
-     :width "500mm"
-     :height "500mm"
-     :viewBox "0 0 500 500"
+     :width (str document-width "mm")
+     :height (str document-height "mm")
+     :viewBox (str "0 0 " document-width " " document-height)
      :xmlns "http://www.w3.org/2000/svg"}
      [:path
       {:d svg-path
@@ -19,19 +27,16 @@
        :fill "transparent"
        }]]))
 
-(def border-radius 5)
-(def padding 7)
-(def width 40)
-(def height 30)
+(defn rounded-curve [xfactor yfactor] (str " a" border-radius "," border-radius " 0 0 1 " (* xfactor border-radius) "," (* yfactor border-radius)))
 
 (def rounded-rectangle-path (str "M" padding "," (+ padding border-radius) 
-" a" border-radius "," border-radius " 0 0 1 " border-radius ",-" border-radius 
+  (rounded-curve 1 -1)
 " h" (- width (* 2 border-radius)) 
-" a" border-radius "," border-radius " 0 0 1 " border-radius "," border-radius 
+  (rounded-curve 1 1)
 " v" (- height (* 2 border-radius))
-" a" border-radius "," border-radius " 0 0 1 -" border-radius "," border-radius
+  (rounded-curve -1 1)
 " h-" (- width (* 2 border-radius) )
-" a" border-radius "," border-radius " 0 0 1 -" border-radius ",-" border-radius
+  (rounded-curve -1 -1)
 " z"
 ))
 
