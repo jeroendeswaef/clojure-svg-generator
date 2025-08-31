6472c4c1fe81101ffd689abfe15c09dea766aa36
Jeroen De Swaef <j@recallq.com>
2025-08-31 13:58:01 +0200

    added latched version of a rounded rectangle for the last cnc passes

---
+++ src/make_svg/latched-rounded-rect.cljs
@@ -0,0 +1,54 @@
+(ns make-svg.core
+  (:require [clojure.string :refer [join]])
+  (:require [clojure.data.xml :as xml])
+  (:require [clojure.math :as math]))
+
+(def border-radius 5)
+(def padding 7)
+(def width 40)
+(def height 30)
+(def latch-width 4)
+
+(def document-width (+ width (* 2 padding)))
+(def document-height (+ height (* 2 padding)))
+
+(defn generate-svg-xml [svg-path]
+  (xml/sexp-as-element
+   [:svg
+    {:version "1.1"
+     :width (str document-width "mm")
+     :height (str document-height "mm")
+     :viewBox (str "0 0 " document-width " " document-height)
+     :xmlns "http://www.w3.org/2000/svg"}
+     [:path
+      {:d svg-path
+       :id "latched-rounded-rectangle"
+       :stroke-width "0.4"
+       :stroke "black"
+       :fill "transparent"
+       }]]))
+
+(defn rounded-curve [xfactor yfactor] (str " a" border-radius "," border-radius " 0 0 1 " (* xfactor border-radius) "," (* yfactor border-radius)))
+
+(def rounded-rectangle-path (str "M" padding "," (+ padding border-radius) 
+  (rounded-curve 1 -1)
+" h" (- (/ (- width latch-width) 2) border-radius) 
+" m" latch-width ",0"
+" h" (- (/ (- width latch-width) 2) border-radius) 
+  (rounded-curve 1 1)
+" v" (- height (* 2 border-radius))
+  (rounded-curve -1 1)
+;; " h-" (- width (* 2 border-radius) )
+" h-" (- (/ (- width latch-width) 2) border-radius) 
+" m-" latch-width ",0"
+" h-" (- (/ (- width latch-width) 2) border-radius) 
+  (rounded-curve -1 -1)
+" v-" (- height (* 2 border-radius))
+))
+
+(defn write-rss! [xml]
+  (with-open [out-file (java.io.FileWriter. "latched-rounded-rectangle.svg")]
+    (xml/emit xml out-file)))
+
+(-> (generate-svg-xml rounded-rectangle-path)
+    write-rss!)
